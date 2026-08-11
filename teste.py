04b254631aadcfcbeda188d7327245ef961d75bb
Elissa <0019887@sesi-es.org.br>
2026-08-11 19:19:28 -0300

    Menu

--- teste.py
+++ teste.py
@@ -1,14 +1,24 @@
-print ("------BIBLIOTECA------")
-print ("1 - Cadastrar")
-print ("2 - Listar")
-print ("3 - Sair")
+livros = []
 
-opcao = input("Digite a opção selecionada: ")
-if opcao == "1":
-    print ("Você escolheu cadastrar")
-elif opcao == "2":
-    print ("Você escolheu listar")
-elif opcao == "3":
-    print ("Saindo da biblioteca...")
-else: 
-    print ("Opção inválida!") 
+while True: 
+    print ("------BIBLIOTECA------")
+    print ("1 - Cadastrar")
+    print ("2 - Listar")
+    print ("3 - Sair")
+
+    opcao = input("Digite a opção selecionada: ")
+
+    if opcao == "1":
+        nome_livro = input ("Digite o nome do livro: ")
+        livros.append(nome_livro)
+        print (f"Livro '{nome_livro}' cadastrado!")
+
+    elif opcao == "2":
+        print (livros)
+
+    elif opcao == "3":
+        print ("Saindo da biblioteca...")
+        break 
+
+    else: 
+        print ("Opção inválida!") 
