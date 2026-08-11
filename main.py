66c442d1c477b4d6053dd1dc1573fa56cb7ab322
Elissa <0019887@sesi-es.org.br>
2026-08-11 18:16:14 -0300

    teste

--- main.py
+++ main.py
@@ -3,4 +3,17 @@ import csv
 ARQUIVO =  'livros.csv'
 def cadastro_livros (titulo, autor, codigo, ano, status = "Disponivel"):
     livros = []
-    
+    livros.append ([titulo, autor, codigo, ano, status])
+ 
+def menu():
+    print("Menu")
+    print("1 - Cadastro de livros" ("Digite a opção selecionada: "))
+    if opcao == 1:
+        titulo = input ("Digite o título: ")
+        autor = input ("Digite o autor: ")
+        codigo = input("Digite o código:")
+        ano = input ("Digite o ano: ")
+        cadastro_livros(titulo,autor,codigo,ano)
+        rint ("livro cadastrado!")
+   
+menu()
---
+++ teste.py
@@ -0,0 +1,14 @@
+print ("------BIBLIOTECA------")
+print ("1 - Cadastrar")
+print ("2 - Listar")
+print ("3 - Sair")
+
+opcao = input("Digite a opção selecionada: ")
+if opcao == "1":
+    print ("Você escolheu cadastrar")
+elif opcao == "2":
+    print ("Você escolheu listar")
+elif opcao == "3":
+    print ("Saindo da biblioteca...")
+else: 
+    print ("Opção inválida!") 
