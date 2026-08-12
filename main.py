5284c80428e4fe33ac37f47d4a20602f323e32ca
Elissa <0019887@sesi-es.org.br>
2026-08-12 19:25:29 -0300

    Cadastro e listagem de livros

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 import csv
 
 ARQUIVO = 'livros.csv'
+
+livros = []
+
 def cadastro_livros (titulo, autor, codigo, ano, status = "Disponivel"):
-    livros = {
+    livro = {
         "titulo": titulo,
-        "autor": autor 
-        "codigo": codigo 
-        "ano": ano 
-        "status": status
+        "autor": autor,
+        "codigo": codigo,
+        "ano": ano, 
+        "status": status,
     }
-    livros.append (livro)
+    livros.append(livro)
  
 def menu():
-    print("Menu")
-    print("1 - Cadastro de livros" ("Digite a opção selecionada: "))
-    if opcao == 1:
-        titulo = input ("Digite o título: ")
-        autor = input ("Digite o autor: ")
-        codigo = input("Digite o código: ")
-        ano = input ("Digite o ano: ")
-        cadastro_livros(titulo, autor, codigo, ano)
-        rint ("Livro cadastrado!")
-   
+    while True:
+        print("Menu")
+        print("1 - Cadastro de livros")
+        print("2 - Listar livros")
+        print("3 - Sair")
+
+        opcao = int(input("Digite a opção selecionada: "))
+
+        if opcao == 1:
+            titulo = input ("Digite o título: ")
+            autor = input ("Digite o autor: ")
+            codigo = input("Digite o código: ")
+            ano = input ("Digite o ano: ")
+
+            cadastro_livros(titulo, autor, codigo, ano)
+            print ("Livro cadastrado!")
+
+        elif opcao == 2: 
+            for livro in livros:
+                print(livro)   
+
+        elif opcao == 3: 
+            print("Programa encerrado!")
+            break
 menu()
